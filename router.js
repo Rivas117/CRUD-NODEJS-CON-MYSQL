f1bd67dc4dde14c8acb0c885f4eb82b48af5ebdc
Rivas117 <Gamer_Luis@Yahoo.com>
2025-09-03 22:18:49 -0600

    Index con Boostrap 5.0

--- router.js
+++ router.js
@@ -4,13 +4,17 @@ const router = express.Router();
 const conexion = require("./Database/db");
 
 router.get("/",(req, res)=>{
-    conexion.query("Select * From Users", (error, results)=>{
+
+    res.render("index.ejs")
+
+
+    /*conexion.query("Select * From Users", (error, results)=>{
        if(error){
         throw error;
        } else{
         res.send(results);
        }
-    })
+    })*/
 })
 
 module.exports = router;
